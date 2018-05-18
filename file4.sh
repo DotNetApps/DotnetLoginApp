aaaea7751b5b7e86a4a723ab1eaa26b66f3a93c1
onlinedevops2018 <39321309+onlinedevops2018@users.noreply.github.com>
2018-05-18 02:22:44 +0000

    added linux basic commands

--- file4.sh
+++ file4.sh
@@ -1,1 +1,7 @@
 shell script
+
+cd - change directory
+
+vi filename - to create a file or edit the existing file
+
+mv <filePath> <newPath> - moving the file/folder from one loaction to another location
